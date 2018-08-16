ef90c80fcd292760e52afb5fd22811d0a12bde96
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-08-16 16:18:12 +0500

    Basic url following added

---
+++ emailcrawler/spiders/emailspider.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import sys
+
+
+class EmailspiderSpider(scrapy.Spider):
+    name = 'emailspider'
+    allowed_domains = ['']
+    start_urls = ['http://google.com/search?q=']
+
+    def start_requests(self):
+        query = input("Enter your query: ")
+        for url in self.start_urls:
+            yield scrapy.Request("{}{}".format(url, query))
+
+    def parse(self, response):
+        
